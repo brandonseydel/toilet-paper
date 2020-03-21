2a6b7f3276ab183c81bf4b7d35311c7cbd47655c
Brandon seydel <brandonseydel@hotmail.com>
2020-03-21 17:11:44 -0500

    fix up some things

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,17 @@ module.exports = function(env) {
             name: '[name].[ext]',
           },
         },
-        {test: /\.(jpe?g|png|gif|svg)$/i, loader: "file-loader"},
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'svg-url-loader',
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
         { test: /\.(scss|css)$/i, use: [ "style-loader", cssLoader, postcssLoader, { loader: "sass-loader", options: { sassOptions: { includePaths: ["node_modules"] } } } ] },
         { test: /\.ts$/i, use: ['ts-loader', '@aurelia/webpack-loader'], exclude: /node_modules/ },
         { test: /\.html$/i, use: '@aurelia/webpack-loader', exclude: /node_modules/ }
